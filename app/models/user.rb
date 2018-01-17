3f80db3b11334250d9ee3b5f525bb24e70f32c8a
zhu qi <ardentyouny@gmail.com>
2018-01-17 22:08:49 +0800

    add post resources

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :posts
+  validates :name, :email, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117125316) do
+ActiveRecord::Schema.define(version: 20180117133652) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
